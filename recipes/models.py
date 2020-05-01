dd98ffe9eb5ca9503848770c6d9aafd8dccc7382
Chelsea White <chewh115@gmail.com>
2020-05-01 12:36:36 -0400

    Updates models.py to not break things by using length instead of max_length, and adds models to migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,26 +1,14 @@
 from django.db import models
 
 # Create your models here.
-"""
-Author
-- name (CharField)
-- bio (TextField)
-
-Recipe
--Title (CharField)
--Author (ForeignKey)
--Description (TextField)
--Time required (CharField, ex: "One hour")
--Instructions (TextField)
-"""
-
 class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField()
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
-    time_required = models.CharField(length=20)
+    time_required = models.CharField(max_length=20)
     instructions = models.TextField()
