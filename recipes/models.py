81f5bf6578ad2c7ac9c1b97f845052bc0e2cdb5d
Chelsea White <chewh115@gmail.com>
2020-05-01 13:13:18 -0400

    Updates to include landing page with recipe titles

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=30)
@@ -12,3 +15,6 @@ class Recipe(models.Model):
     description = models.TextField()
     time_required = models.CharField(max_length=20)
     instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from recipes.models import Recipe
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    data = Recipe.objects.all()
+    return render(request, 'index.html', {'data': data})
