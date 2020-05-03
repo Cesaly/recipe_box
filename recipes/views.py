b6c0796e4dc621855680b11463ae7813a6d79c2e
Chelsea White <chewh115@gmail.com>
2020-05-03 13:12:19 -0400

    Updates views to include recipe and author

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
-from recipes.models import Recipe
+from recipes.models import Author, Recipe
 
 # Create your views here.
 def index(request):
     data = Recipe.objects.all()
     return render(request, 'index.html', {'data': data})
+
+def recipe(request, title):
+    data = Recipe.objects.get(title=title)
+    return render(request, 'recipe.html', {'data': data})
+
+def author(request, name):
+    author = Author.objects.get(name=name)
+    data = Recipe.objects.get(author=author)
+    return render(request, 'author.html', {'data': data})
