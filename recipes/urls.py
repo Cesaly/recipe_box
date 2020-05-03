f43219c67ae07f4f0b7b5448236e36a228e7e0d1
Chelsea White <chewh115@gmail.com>
2020-05-03 13:32:43 -0400

    Updates urls.py to have working formatting for urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.index)
-    # path('admin/', admin.site.urls),
+    path('', views.index),
+    path('recipe/<str:title>/', views.recipe),
+    path('author/<str:name>/', views.author)
 ]
