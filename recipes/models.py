fb9aee66a3835e9da9e053e06edcedc615477310
Chelsea White <chewh115@gmail.com>
2020-05-08 14:54:16 -0400

    Updates to connect user to Author model, and adds ingredients field for recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -13,6 +15,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
+    ingredients = models.TextField()
     time_required = models.CharField(max_length=20)
     instructions = models.TextField()
 
