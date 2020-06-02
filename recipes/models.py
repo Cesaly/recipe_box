1bb220bb0790994d576435eb2440654be7af00c7
Chelsea White <chewh115@gmail.com>
2020-06-02 12:12:05 -0400

    Likes was breaking things, added null=True to model + migrated

--- recipes/models.py
+++ recipes/models.py
@@ -18,7 +18,7 @@ class Recipe(models.Model):
     ingredients = models.TextField()
     time_required = models.CharField(max_length=20)
     instructions = models.TextField()
-    likes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0, null=True)
 
     def __str__(self):
         return self.title
